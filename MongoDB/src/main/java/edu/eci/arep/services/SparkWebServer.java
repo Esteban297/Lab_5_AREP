467d5421ace0bc7bf3e71e1ba93053fab1cc42c2
Esteban297 <estebancristancho9@gmail.com>
2023-03-09 23:03:03 -0500

    lab terminado

---
+++ MongoDB/src/main/java/edu/eci/arep/services/SparkWebServer.java
@@ -0,0 +1,53 @@
+package edu.eci.arep.services;
+
+import edu.eci.arep.services.ConnectionDB.MongoDBConnection;
+
+import static spark.Spark.*;
+public class SparkWebServer {
+    public static void main( String[] args ) {
+        // Database connection and operations
+        MongoDBConnection mongoDB = new MongoDBConnection();
+
+        port(getPort());
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+        get("/savecadena", (req, res) -> {
+            res.type("application/json");
+            String value = req.queryParams("cadena");
+            System.out.println("Esta entrando aca");
+            if(value == null) {
+                return null;
+            } else {
+                try{
+                    return mongoDB.createConexion("AREP", value);
+                }catch (NumberFormatException e) {
+                    return null;
+                }
+            }
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+
+}
